cdf516ef5f6638bfc0855993c2c044601da97277
Giovane Ferreira <giovaneferreira93@gmail.com>
2019-11-09 15:53:18 -0300

    escrevendo testes para requisições get

---
+++ tests/Unit/PoolTest.php
@@ -0,0 +1,49 @@
+<?php
+
+class PoolTest extends TestCase
+{
+    public function testCanListPools() {
+        $response = $this->call('GET', '/pools');
+        $responseData = json_decode($response->content(), true);
+
+        $this->assertEquals(200, $response->status());
+        $this->assertTrue(is_array($responseData));
+    }
+
+    public function testCanViewPoolData() {
+        $response = $this->call('GET', '/pools/1');
+        $responseData = json_decode($response->content(), true);
+
+        $this->assertEquals(200, $response->status());
+        $this->assertArrayHasKey('id', $responseData);
+        $this->assertArrayHasKey('description', $responseData);
+        $this->assertArrayHasKey('options', $responseData);
+    }
+
+    public function testCanViewPoolStats() {
+        $response = $this->call('GET', '/pools/1/stats');
+        $responseData = json_decode($response->content(), true);
+
+        $this->assertEquals(200, $response->status());
+        $this->assertArrayHasKey('views', $responseData);
+        $this->assertArrayHasKey('votes', $responseData);
+    }
+
+    // public function testCanCreatePool()
+    // {
+    //     $data = [
+    //         'description' => 'Test description of the pool',
+    //         'options' => [
+    //             'Test description of option 1',
+    //             'Test description of option 2',
+    //             'Test description of option 3',
+    //         ],
+    //     ];
+
+    //     $response = $this->call('POST', '/pools', $data);
+    //     $responseData = json_decode($response->content(), true);
+
+    //     $this->assertEquals(201, $response->status());
+    //     $this->assertArrayHasKey('id', $responseData);
+    // }
+}
